430fb48b373da10e6314df895bae9facaab012b1
Prash Katukojwala <31807747+prashasthk@users.noreply.github.com>
2022-06-12 18:46:02 -0700

    week 1 lab tasks

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profile from "../assets/Profile.jpeg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={profile}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Prash Katukojwala</div>
+        <div className="brief_description">
+          UC San Diego Computer Science 2025
+        </div>
+      </div>
+    </div>
+  </div>
       </div>
     )
   }
